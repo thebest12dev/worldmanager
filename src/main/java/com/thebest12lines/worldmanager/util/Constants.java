c3d7a3db1c3dcab2c95c71304af2849051a9fc26
thebest12lines <userunamed981+github@gmail.com>
2024-09-27 18:20:46 +0600

    Added other code coverage things

--- src/main/java/com/thebest12lines/worldmanager/util/Constants.java
+++ src/main/java/com/thebest12lines/worldmanager/util/Constants.java
@@ -5,29 +5,29 @@ public class Constants {
     public static enum UpdateCheckResult {
         UPDATE_NEEDED,UP_TO_DATE
     }
-    public static enum WorldReadResult {
-        SUCCESS,FAIL,UNKNOWN
-    }
-    public static enum WorldBackupResult {
-        INSUFFICIENT_DISK_SPACE, BACKED_UP, FAIL, UNKNOWN
-    }
+    // public static enum WorldReadResult {
+    //     SUCCESS,FAIL,UNKNOWN
+    // }
+    // public static enum WorldBackupResult {
+    //     INSUFFICIENT_DISK_SPACE, BACKED_UP, FAIL, UNKNOWN
+    // }
     public static enum FeatureLoadResult {
         LOADED,CANNOT_FIND_METHOD,CANNOT_FIND_CLASS,NOT_LOADED
     }
-    public static enum BackupType {
-        TEMPORARY, PERMANENT
-    }
-    public static enum WorldVersionType {
-        PRE_1_2_1, POST_1_2_1
-    }
-    public static enum FeatureType {
-        BUILT_IN_FEATURE,PLUGIN_FEATURE,EXTERNAL_LIBRARY
-    }
-    public static enum WorldEditionType {
-        JAVA,BEDROCK
-    }
-    public static enum WorldBackupResolveType {
-        GLOBAL_BACKUPS,IN_WORLD_BACKUPS
-    }
+    // public static enum BackupType {
+    //     TEMPORARY, PERMANENT
+    // }
+    // public static enum WorldVersionType {
+    //     PRE_1_2_1, POST_1_2_1
+    // }
+    // public static enum FeatureType {
+    //     BUILT_IN_FEATURE,PLUGIN_FEATURE,EXTERNAL_LIBRARY
+    // }
+    // public static enum WorldEditionType {
+    //     JAVA,BEDROCK
+    // }
+    // public static enum WorldBackupResolveType {
+    //     GLOBAL_BACKUPS,IN_WORLD_BACKUPS
+    // }
     
 }
--- src/test/java/main/UpdateTest.java
+++ src/test/java/main/UpdateTest.java
@@ -1,7 +1,43 @@
 package main;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.Test;
 
+import com.thebest12lines.worldmanager.util.UpdateBuildException;
+import com.thebest12lines.worldmanager.util.Updater;
+
 public class UpdateTest {
-    
+    @Test
+    public void testDeleteUpdate() {
+        
+            new File("worldmanager_0.jar").delete();
+         
+    }
+    @Test
+    public void testUpdateCheck() {
+        try {
+            Updater.checkForUpdates();
+        } catch (UpdateBuildException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void testUpdateDownload() {
+        try {
+            Updater.downloadAndInstallUpdates();
+            
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } 
+        
+    }
+     
 }
