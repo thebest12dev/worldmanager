8ec5f07a76fc3582403795a45a28201fbca299d2
thebest12lines <userunamed981+github@gmail.com>
2024-09-28 19:25:39 +0600

    commit

---
+++ src/main/java/com/thebest12lines/worldmanager/DataManager.java
@@ -0,0 +1,81 @@
+package com.thebest12lines.worldmanager;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DataManager {
+
+    private static JSONObject json;
+    public static void setVersion(String version) {
+        if (json == null) {
+            initialize();
+        }
+        json.getJSONObject("worldmanagerMeta").put("version", version);
+        try {
+            Files.write(new File("worldmanager.json").toPath(), json.toString(4).getBytes());
+        } catch (JSONException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+    }
+    public static void setBuild(int build) {
+        if (json == null) {
+            initialize();
+        }
+        json.getJSONObject("worldmanagerMeta").put("build", build);
+        try {
+            Files.write(new File("worldmanager.json").toPath(), json.toString(4).getBytes());
+        } catch (JSONException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    public static int getBuild() {
+        if (json == null) {
+            initialize();
+        }
+        return json.getJSONObject("worldmanagerMeta").getInt("build");
+    }
+    public static String getVersion() {
+        if (json == null) {
+            initialize();
+        }
+        return json.getJSONObject("worldmanagerMeta").getString("version");
+    }
+    public static String getBranch() {
+        if (json == null) {
+            initialize();
+        }
+        return json.getJSONObject("worldmanagerMeta").getString("branch");
+    }
+    public static String getFullVersion() {
+        if (json == null) {
+            initialize();
+        }
+        StringBuilder builder = new StringBuilder();
+        builder.append(getBranch()+"-"+getVersion());
+        builder.append("+build_"+getBuild());
+        return builder.toString();
+    }
+        
+    private static void initialize() {
+        StringBuilder builder = new StringBuilder();
+        try {
+            Files.lines(Paths.get("worldmanager.json")).forEach(builder::append);
+            json = new JSONObject(builder.toString());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(getFullVersion());
+    }
+}
---
+++ src/main/java/com/thebest12lines/worldmanager/gui/FlatPopupMenu.java
@@ -0,0 +1,24 @@
+package com.thebest12lines.worldmanager.gui;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPopupMenu;
+
+public class FlatPopupMenu {
+    public static JPopupMenu createFlatPopupMenu() {
+        JPopupMenu menu = new JPopupMenu() {
+            @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.setColor(new Color(0, 0, 0, 50));
+        g2d.fillRoundRect(5, 5, getWidth() - 10, getHeight() - 10, 15, 15);
+        g2d.dispose();
+    }
+        };
+
+        return menu;
+    }
+}
